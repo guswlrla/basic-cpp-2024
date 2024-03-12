54ebc2081d2e098da27383f8c38b9ffe9ef0c097
guswlrla <rlaguswl_01@naver.com>
2024-03-12 17:30:45 +0900

    update

---
+++ day14/Project1/Project1/test01.cpp
@@ -0,0 +1,17 @@
+/*
+  char ary[] = {'a', 'b', 'c', 'd', 'e', 'f'};
+  조건: while문과 if문만으로 배열 순서를 바꾸어 저장 출력하시오.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char ary[] = { 'a', 'b', 'c', 'd', 'e', 'f' };
+	int size = sizeof(ary) / sizeof(ary[0]);
+	int start, end;
+
+	return 0;
+}
